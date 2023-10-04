0f29336ff8f497f5a1a510c3298f76b080411cab
Stefano Calabretti <StefanoCalabretti@outlook.it>
2023-10-04 17:44:06 +0200

    Add DI package

---
+++ src/Webion.Extensions.DependencyInjection/IModule.cs
@@ -0,0 +1,29 @@
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Cecir.Application.Extensions;
+
+
+public interface IModule
+{
+    void Configure(IServiceCollection services);
+}
+
+public static class ModuleExtension
+{
+    public static void AddModulesFromAssembly<T>(this IServiceCollection services)
+    {
+        var assembly = typeof(T).Assembly;
+        var moduleTypes = assembly
+            .GetTypes()
+            .Where(t => t is { IsPublic: true, IsAbstract: false, IsClass: true })
+            .Where(t => t.GetInterface(nameof(IModule)) == typeof(IModule));
+
+        foreach (var type in moduleTypes)
+        {
+            if (Activator.CreateInstance(type) is not IModule module)
+                throw new InvalidOperationException("Module must implement IModule interface");
+
+            module.Configure(services);
+        }
+    }
+}
