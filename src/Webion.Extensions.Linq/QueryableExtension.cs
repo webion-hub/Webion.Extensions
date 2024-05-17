b32e922f3cdc4a3e715d46cf20c34a5401b98aa4
Davide Messori <41765255+davidemesso@users.noreply.github.com>
2024-05-17 10:55:20 +0200

    feat: Update Webion.Extensions.Linq with else version for If (#4)

--- src/Webion.Extensions.Linq/QueryableExtension.cs
+++ src/Webion.Extensions.Linq/QueryableExtension.cs
@@ -9,6 +9,15 @@ public static class QueryableExtension
     {
         return condition ? func(query) : query;
     }
+    
+    public static IQueryable<T> If<T>(this IQueryable<T> query,
+        bool condition,
+        Func<IQueryable<T>, IQueryable<T>> then,
+        Func<IQueryable<T>, IQueryable<T>> orElse
+    )
+    {
+        return condition ? then(query) : orElse(query);
+    }
 
 
     public static IQueryable<T> Transform<T>(this IQueryable<T> query,
