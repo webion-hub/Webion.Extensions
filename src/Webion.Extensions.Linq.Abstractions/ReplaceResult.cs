d738bc1394f6b05e7b0330c3c318e787b96e46ff
Stefano Calabretti <stefano.calabretti@webion.it>
2024-05-20 18:18:18 +0200

    feat: Update SoftReplace functionality and target frameworks
    Upgraded the 'SoftReplace' method in the Webion.Extensions.Linq project, providing more control over add, update, and delete operations. Also, updated project configuration to target both .NET 7.0 and 8.0 versions. The detailed summary documentation was added to 'SoftReplace' method and 'ReplaceResult' class to improve code readability.

--- src/Webion.Extensions.Linq.Abstractions/ReplaceResult.cs
+++ src/Webion.Extensions.Linq.Abstractions/ReplaceResult.cs
@@ -1,3 +1,47 @@
 namespace Webion.Extensions.Linq.Abstractions;
 
-public sealed record ReplaceResult<T1, T2>(List<T1> Removed);
+/// <summary>
+/// Represents the result of a soft replace operation.
+/// </summary>
+/// <typeparam name="T1">The type of the elements in the original collection.</typeparam>
+/// <typeparam name="T2">The type of the elements in the replacement collection.</typeparam>
+public sealed class ReplaceResult<T1, T2>
+{
+    /// <summary>
+    /// Gets the list of items that were added during the replace operation.
+    /// </summary>
+    /// <typeparam name="T2">The type of the elements in the replacement collection.</typeparam>
+    /// <value>
+    /// The list of added items.
+    /// </value>
+    public required List<T2> Added { get; init; }
+
+    /// <summary>
+    /// Gets the list of items that were created during the replace operation.
+    /// </summary>
+    /// <typeparam name="T1">The type of the elements in the original collection.</typeparam>
+    /// <value>
+    /// The list of created items.
+    /// </value>
+    public required List<T1> Created { get; init; }
+
+
+    /// <summary>
+    /// Gets the list of items that were updated during the replace operation.
+    /// </summary>
+    /// <typeparam name="T1">The type of the elements in the original collection.</typeparam>
+    /// <value>
+    /// The list of updated items.
+    /// </value>
+    public required List<T1> Updated { get; init; }
+
+
+    /// <summary>
+    /// Gets the list of items that were removed during the replace operation.
+    /// </summary>
+    /// <typeparam name="T1">The type of the elements in the original collection.</typeparam>
+    /// <value>
+    /// The list of removed items.
+    /// </value>
+    public required List<T1> Removed { get; init; }
+}
--- src/Webion.Extensions.Linq/SoftReplaceExtension.cs
+++ src/Webion.Extensions.Linq/SoftReplaceExtension.cs
@@ -4,43 +4,106 @@ namespace Webion.Extensions.Linq;
 
 public static class CollectionExtensions
 {
+    /// <summary>
+    /// Soft replaces the elements in the collection with the specified replacement collection based on a matching criterion.
+    /// </summary>
+    /// <remarks>
+    /// If <c>skipAdd</c>, <c>skipUpdate</c> or <c>skipDelete</c> are used, the method will not loop during the given phase,
+    /// meaning that the corresponding lists inside the result will be empty.
+    /// </remarks>
+    /// <typeparam name="T1">The type of the elements in the original collection.</typeparam>
+    /// <typeparam name="T2">The type of the elements in the replacement collection.</typeparam>
+    /// <param name="collection">The collection to soft replace the elements in.</param>
+    /// <param name="replacement">The replacement collection.</param>
+    /// <param name="match">The matching criterion used to determine whether an element in the original collection should be replaced.</param>
+    /// <param name="add">
+    /// The function that is invoked to add a new element to the collection. <br/>
+    /// If set to <c>null</c>, the <see cref="ReplaceResult{T1,T2}.Created"/> property of the result will be empty, as no mapping occurs.<br/>
+    /// Defaults to <c>null</c>.
+    /// </param>
+    /// <param name="update">The action that is invoked to update an existing element in the collection. Defaults to <c>null</c>.</param>
+    /// <param name="delete">The action that is invoked to delete a removed element from the collection. Defaults to <c>null</c>.</param>
+    /// <param name="skipAdd">
+    /// Indicates whether to skip adding new elements to the collection.<br/>
+    /// If set to <c>false</c>, the <see cref="ReplaceResult{T1,T2}.Added"/> and <see cref="ReplaceResult{T1,T2}.Created"/> properties
+    /// inside the result will be left empty.<br/>
+    /// Defaults to <c>false</c>
+    /// </param>
+    /// <param name="skipUpdate">
+    /// Indicates whether to skip updating existing elements in the collection.<br/>
+    /// If set to <c>false</c>, the <see cref="ReplaceResult{T1,T2}.Updated"/> property inside the result will be left empty.<br/>
+    /// Defaults to <c>false</c>
+    /// </param>
+    /// <param name="skipDelete">
+    /// Indicates whether to skip deleting removed elements from the collection.<br/>
+    /// If set to <c>false</c>, the <see cref="ReplaceResult{T1,T2}.Removed"/> property inside the result will be left empty.<br/>
+    /// Defaults to <c>false</c>
+    /// </param>
+    /// <returns>A <see cref="ReplaceResult{T1, T2}"/> object that represents the result of the soft replace operation.</returns>
     public static ReplaceResult<T1, T2> SoftReplace<T1, T2>(this ICollection<T1> collection, 
         IEnumerable<T2> replacement,
         Func<T1, T2, bool> match,
-        Func<T2, T1> add,
-        Action<T1, T2> update,
-        Action<T1> delete
+        Func<T2, T1>? add = default,
+        Action<T1, T2>? update = default,
+        Action<T1>? delete = default,
+        bool skipAdd = false,
+        bool skipUpdate = false,
+        bool skipDelete = false
     )
     {
         var newItems = replacement.ToList();
-        var removed = new List<T1>();
+        var deleted = new List<T1>();
+        var created = new List<T1>();
+        var updated = new List<T1>();
+        var added = new List<T2>();
         
         foreach (var existingItem in collection)
         {
             var newItem = newItems.FirstOrDefault(n => match(existingItem, n));
-            if (newItem != null)
+            if (newItem is not null)
             {
-                update(existingItem, newItem);
+                if (skipUpdate)
+                    continue;
+                
+                update?.Invoke(existingItem, newItem);
+                updated.Add(existingItem);
             }
-            else
+            else if (!skipDelete)
             {
-                removed.Add(existingItem);
+                deleted.Add(existingItem);
             }
         }
 
-        foreach (var newItem in newItems)
+        if (!skipAdd)
         {
-            var existingItem = collection.FirstOrDefault(s => match(s, newItem));
-            if (existingItem != null)
-                continue;
-            
-            var addedItem = add(newItem);
-            collection.Add(addedItem);
-        }
-        
-        foreach (var d in removed)
-            delete(d);
+            foreach (var newItem in newItems)
+            {
+                var existingItem = collection.FirstOrDefault(s => match(s, newItem));
+                if (existingItem is not null)
+                    continue;
 
-        return new ReplaceResult<T1, T2>(removed);
+                added.Add(newItem);
+                if (add is not null)
+                {
+                    var addedItem = add(newItem);
+                    collection.Add(addedItem);
+                    created.Add(addedItem);
+                }
+            }
+        }
+
+        if (!skipDelete && delete is not null)
+        {
+            foreach (var d in deleted)
+                delete(d);
+        }
+
+        return new ReplaceResult<T1, T2>
+        {
+            Added = added,
+            Created = created,
+            Updated = updated,
+            Removed = deleted,
+        };
     }
 }
