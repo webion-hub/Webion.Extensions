20b24c242100606752d20343f44ece2524255cee
Stefano Calabretti <stefano.calabretti@webion.it>
2024-10-26 14:16:14 +0200

    Add FluentValidation extension for ASP.NET Core
    Introduce a new project with an extension method for asynchronous model validation using FluentValidation. This addition includes project files and a brief README for guidance.

---
+++ src/Webion.Extensions.AspNetCore.FluentValidation/FluentValidationExtension.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace Webion.Extensions.AspNetCore.FluentValidation;
+
+public static class FluentValidationExtension
+{
+    public static async Task ValidateModelAsync<T>(this AbstractValidator<T> validator,
+        T request,
+        ModelStateDictionary modelState,
+        CancellationToken cancellationToken = default
+    )
+    {
+        var result = await validator.ValidateAsync(request, cancellationToken);
+        result.AddToModelState(modelState);
+    }
+}
---
+++ src/Webion.Extensions.AspNetCore.FluentValidation/README.md
@@ -0,0 +1,1 @@
+Contains the extension method `ModelState.ValidateModelAsync`
