59b7418e71031c0bfbced7ff6f9d59328b5832b2
Francesco Fiume <franzfiume97@gmail.com>
2025-05-20 14:06:03 +0200

    Patch 1 (#6)
    * Update ClickUpDefaults.cs
    
    * Update Webion.Extensions.AspNetCore.Authentication.ClickUp.csproj
    
    * Update ClickUpDefaults.cs
    
    * Update Webion.Extensions.AspNetCore.Authentication.ClickUp.csproj

--- src/clickup/Webion.Extensions.AspNetCore.Authentication.ClickUp/ClickUpDefaults.cs
+++ src/clickup/Webion.Extensions.AspNetCore.Authentication.ClickUp/ClickUpDefaults.cs
@@ -4,7 +4,7 @@ public static class ClickUpDefaults
 {
     public const string AuthenticationScheme = "ClickUp";
     public const string DisplayName = "ClickUp";
-    public const string AuthorizationEndpoint = "https://api.clickup.com/api";
+    public const string AuthorizationEndpoint = "https://app.clickup.com/api";
     public const string TokenEndpoint = "https://api.clickup.com/api/v2/oauth/token";
     public const string UserInformationEndpoint = "https://api.clickup.com/api/v2/user";
 }
